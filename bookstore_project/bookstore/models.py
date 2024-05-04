49d4a0cd6e7b289530695e2491c27e08bc024018
Adel <ahmeddadel9988@gmail.com>
2024-05-04 20:42:14 +0300

    BookStore

---
+++ bookstore_project/bookstore/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    published_date = models.DateField()
